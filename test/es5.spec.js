c2100508484b35197bd797daf6c228449fb87309
hieunthkp <hieu.nguyen@4digit.com>
2022-04-04 16:52:03 +0700

    fix: spec files to work with eslint 8

--- test/es5.spec.js
+++ test/es5.spec.js
@@ -1,22 +1,22 @@
 const { resolve } = require('path');
-const { CLIEngine } = require('eslint');
+const { ESLint } = require('eslint');
 
-const linter = new CLIEngine({ extends: ['./base.js'] });
+const linter = new ESLint({ baseConfig: { extends: ['./base.js'] } });
 
 describe('bads', () => {
   const filepath = resolve(__dirname, 'es5/bad.js');
-  const subject = linter.executeOnFiles([filepath]);
+  const results = linter.lintFiles([filepath]);
 
   it('should fail eslint verifying', () => {
-    expect(subject).not.toHaveProperty('errorCount', 0);
+    results.then((res) => expect(res[0].errorCount).toBeGreaterThan(0));
   });
 });
 
 describe('goods', () => {
   const filepath = resolve(__dirname, 'es5/good.js');
-  const subject = linter.executeOnFiles([filepath]);
+  const results = linter.lintFiles([filepath]);
 
   it('should pass eslint verifying', () => {
-    expect(subject).toHaveProperty('errorCount', 0);
+    results.then((res) => expect(res[0].errorCount).toBe(0));
   });
 });
--- test/esnext.spec.js
+++ test/esnext.spec.js
@@ -1,22 +1,22 @@
 const { resolve } = require('path');
-const { CLIEngine } = require('eslint');
+const { ESLint } = require('eslint');
 
-const linter = new CLIEngine({ extends: ['./esnext.js'] });
+const linter = new ESLint({ baseConfig: { extends: ['./esnext.js'] } });
 
 describe('bads', () => {
-  const filepath = resolve(__dirname, 'esnext/bad.js')
-  const subject = linter.executeOnFiles([filepath]);
+  const filepath = resolve(__dirname, 'esnext/bad.js');
+  const results = linter.lintFiles([filepath]);
 
   it('should fail eslint verifying', () => {
-    expect(subject).not.toHaveProperty('errorCount', 0);
+    results.then((res) => expect(res[0].errorCount).toBeGreaterThan(0));
   });
 });
 
 describe('goods', () => {
-  const filepath = resolve(__dirname, 'esnext/good.js')
-  const subject = linter.executeOnFiles([filepath]);
+  const filepath = resolve(__dirname, 'esnext/good.js');
+  const results = linter.lintFiles([filepath]);
 
   it('should pass eslint verifying', () => {
-    expect(subject).toHaveProperty('errorCount', 0);
+    results.then((res) => expect(res[0].errorCount).toBe(0));
   });
 });
